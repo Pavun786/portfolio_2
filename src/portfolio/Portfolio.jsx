e2ad12b785e8ed2c99ce85f16d5d292fa37c96e9
Pavun786 <pulsarpavuns786@gmail.com>
2024-01-08 11:24:50 -0800

    project4 add

--- src/portfolio/Portfolio.jsx
+++ src/portfolio/Portfolio.jsx
@@ -64,6 +64,20 @@ const Portfolio = () => {
 
         </div>
 
+        <div className="card" style={{ border: darkMode ? '1px solid grey' : '' }}>
+          <img src="assets/projects/Project4.png" alt="Project4 img" />
+          <h3 style={{ color: darkMode ? 'white' : '' }}>Social Media Application</h3>
+          <p>React,Nodejs,Expressjs,MongoDB</p>
+          <p style={{ color: 'grey', textAlign: 'center', fontSize: '14px' }} className='proj-description'>A social media application with users, posts, likes and comments - developed using React, Node, Express and MongoDB.</p>
+          <div className="sourcecode-icons">
+            <a href="https://github.com/Pavun786/Social_Media-FrontEnd.git" target="_blank">< AiOutlineGithub style={{ fontSize: '30px' }} className='ai-icon' /></a>
+
+            <a href="https://social-media-front-end-beta.vercel.app/" target="_blank"><RiLiveLine style={{ fontSize: '30px' }} className='ai-icon' /></a>
+
+          </div>
+
+        </div>
+
       </div>
     </div>
   )
